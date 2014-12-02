702af7ebb6891647281047031daa974cc0f1150e
shaalx <1316011195@qq.com>
2014-12-02 13:56:28 +0800

    commit only

---
+++ commit_only.bat
@@ -0,0 +1,9 @@
+git add -A
+::echo commit for change : 
+set /p commit_for_change=
+git commit -m "%commit_for_change%"
+::git push osc master
+::git push origin master
+::net stop server
+ping /n 8 127.1 >nul
+::net start server
