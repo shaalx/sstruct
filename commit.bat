de8d099db385e7e544711de1b4eabd1e95250c86
shaalx <wisdomswing@gmail.com>
2014-12-02 20:42:32 +0800

    commit

--- commit.bat
+++ commit.bat
@@ -1,10 +1,10 @@
-git status
 git add -A
+git status
 ::echo commit for change : 
 set /p commit_for_change=
 git commit -m "%commit_for_change%"
-git push osc master
-git push origin master
+git push oschina master
+git push github master
 ::net stop server
 ping /n 8 127.1 >nul
 ::net start server
