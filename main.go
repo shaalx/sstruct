f53c0ba7b5324f3e87ca3b43def739ffb38b3d2e
shaalx <wisdomswing@gmail.com>
2014-12-03 17:45:34 +0800

    fetch Data

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	url := "https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewRoom?fcId=560264462&genreIdString=36&mediaTypeString=Mobile+Software+Applications"
+	url := "http://fanyi.youdao.com/openapi.do?keyfrom=sasfasdfasf&key=1177596287&type=data&doctype=json&version=1.1&q=love"
+	// url := "https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewRoom?fcId=560264462&genreIdString=36&mediaTypeString=Mobile+Software+Applications"
 	ipaddr := "202.120.87.152"
 	bs := fetch.Do(url, ipaddr)
 	fmt.Println(string(bs))
