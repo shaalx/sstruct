2cb746f07fed8d3c1d0655a44c1b16f35377f0f2
shaalx <1316011195@qq.com>
2014-12-04 14:45:49 +0800

    action kuwo

--- action/kuwo_mgo.go
+++ action/kuwo_mgo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/shaalx/sstruct/fetch"
 	"github.com/shaalx/sstruct/persistence"
+	"strings"
 )
 
 type KuwoMgoAction struct {
@@ -11,11 +12,16 @@ type KuwoMgoAction struct {
 }
 
 func (z *KuwoMgoAction) Do() {
-	url := "http://qukudata.kuwo.cn/q.k?op=query&cont=ninfo&node=26&pn=0&rn=10&fmt=json&src=pad&callback=showNodeChild&r=2014123"
+	url := "http://artistlistinfo.kuwo.cn/mb.slist?stype=artistlist&category=0&order=hot&pn=0&rn=20&callback=showAreaArtistList&r=1417619717508"
 	ipaddr := "202.120.87.152"
 	bs := fetch.Do(url, ipaddr)
-	fmt.Println(string(bs))
+	stmp := string(bs)
+	stmp = strings.Replace(stmp, "try {var jsondata=", "", -1)
+	stmp = strings.Replace(stmp, ";showAreaArtistList(jsondata);}catch(e){jsonError(e)}", "", -1)
+
+	stmp = strings.Replace(stmp, "'total':'49522','pn':'0','rn':'20','category':'0','new_album':'1','new_album_cnt':'107','artistlist':", "", -1)
+	fmt.Println(string(stmp))
 	var p persistence.MgoPersistence
 	p.Server = []string{"", "newsmgo", "kuwo"}
-	p.Do(bs)
+	p.Do([]byte(stmp))
 }
