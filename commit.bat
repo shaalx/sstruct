222b14f855a547736d5505e33170f5ca949ed856
shaalx <1316011195@qq.com>
2014-12-08 09:49:32 +0800

    commit script

--- commit.bat
+++ commit.bat
@@ -3,8 +3,8 @@ git status
 ::echo commit for change : 
 set /p commit_for_change=
 git commit -m "%commit_for_change%"
-git push oschina master
-git push github master
+git push oschina sstruct:sstruct
+git push github sstruct:sstruct
 ::net stop server
 ping /n 8 127.1 >nul
 ::net start server
