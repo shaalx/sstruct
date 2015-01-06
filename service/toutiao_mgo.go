362a5ea491a9213347e170428dba642c625720b3
shaalx <1316011195@qq.com>
2015-01-06 16:37:31 +0800

    s

--- service/toutiao_mgo.go
+++ service/toutiao_mgo.go
@@ -23,7 +23,8 @@ func (self *ToutiaoAction) Init() {
 }
 
 func (self *ToutiaoAction) Persistence() {
-	url := "http://ic.snssdk.com/2/article/v21/stream/?iid=2463135716&city=%E5%8D%97%E4%BA%AC%E5%B8%82%E5%B8%82%E8%BE%96%E5%8C%BA&longitude=121.3861245975094&latitude=31.23194430763986&user_city=%E5%8D%97%E4%BA%AC&min_behot_time=0&detail=1&image=1&count=20&ac=wifi&channel=App%20Store&app_name=news_article&aid=13&version_code=4.2&device_platform=ipad&os_version=8.1&device_type=iPad%20Mini%20Retina&vid=0256CB17-8BBE-4974-B25D-B4691079ACDC&openudid=1663351e7f057fe184db98ac159e9971e590aef8&idfa=D5A1F8CF-75C5-4DB9-8C3E-CCD702148275"
+	url := "http://toutiao.com/api/article/recent/?callback=jQuery17206307146116159856_1420533186372&category=news_society&count=20&max_behot_time=1420533186.73&offset=0&utm_source=toutiao&_=1420533189249"
+	// url := "http://ic.snssdk.com/2/article/v21/stream/?iid=2463135716&city=%E5%8D%97%E4%BA%AC%E5%B8%82%E5%B8%82%E8%BE%96%E5%8C%BA&longitude=121.3861245975094&latitude=31.23194430763986&user_city=%E5%8D%97%E4%BA%AC&min_behot_time=0&detail=1&image=1&count=20&ac=wifi&channel=App%20Store&app_name=news_article&aid=13&version_code=4.2&device_platform=ipad&os_version=8.1&device_type=iPad%20Mini%20Retina&vid=0256CB17-8BBE-4974-B25D-B4691079ACDC&openudid=1663351e7f057fe184db98ac159e9971e590aef8&idfa=D5A1F8CF-75C5-4DB9-8C3E-CCD702148275"
 	// url := "http://ic.snssdk.com/2/article/v10/hot_comments/?iid=2463135716&ac=wifi&channel=App%20Store&app_name=news_article&aid=13&version_code=4.2&device_platform=ipad&os_version=8.1&device_type=iPad%20Mini%20Retina&vid=0256CB17-8BBE-4974-B25D-B4691079ACDC&openudid=1663351e7f057fe184db98ac159e9971e590aef8&idfa=D5A1F8CF-75C5-4DB9-8C3E-CCD702148275"
 	ipaddr := "202.120.87.152"
 	bs := fetch.Do(url, ipaddr)
