afa344c03f0cf0718c6ca11c67846478718340a0
shaalx <wisdomswing@gmail.com>
2015-03-11 20:44:48 +0800

    topic

--- service/topic.go
+++ service/topic.go
@@ -9,6 +9,7 @@ import (
 	// "github.com/shaalx/sstruct/bean"
 	"github.com/shaalx/sstruct/service/search"
 	"github.com/shaalx/sstruct/utils"
+	"sort"
 	"time"
 )
 
@@ -17,6 +18,12 @@ type Topic struct {
 	Const  string
 	Relate string
 	Parent int64
+}
+
+func (t Topic) String() string {
+	// tStr := fmt.Sprintf("Id %d\t ,Const %s\t ,Relate %s ,Parent %d\n", t.Id, t.Const, t.Relate, t.Parent)
+	tStr := fmt.Sprintf("%d\t %s\t %d\t %s\n", t.Id, t.Relate, t.Parent, t.Const)
+	return tStr
 }
 
 type TopicMap map[string]Topic
@@ -50,43 +57,42 @@ func (self *TopicAction) QueryOne() {
 
 func (self *TopicAction) Analyse() {
 	one := self.persis.QuerySortedNewsOne(nil, "-unixdate")
-	// buf := utils.I2Bytes(one.Content)
-	fmt.Println(one.DisplayDate)
-	// fmt.Println(one.Content)
 
-	// 第一层数组
+	stringSaveChan = make(chan string, 5)
+	go utils.SaveString(stringSaveChan)
+
 	bsfirst := utils.I2Bytes(one.Content)
-	contentArrayOfFirstLayer := search.SearchArrays(bsfirst, []string{}...)
-	// fmt.Println(contentArrayOfFirstLayer)
+	self.analyse("sentence", bsfirst)
+	/*	// 第一层数组
+		contentArrayOfFirstLayer := search.SearchArrays(bsfirst, []string{}...)
 
-	// 第二层数组
-	bssecond := utils.I2Bytes(contentArrayOfFirstLayer[0])
-	contentArrayOfSecondLayer := search.SearchArrays(bssecond, []string{}...)
-	// fmt.Println(contentArrayOfSecondLayer)
+		// 第二层数组
+		bssecond := utils.I2Bytes(contentArrayOfFirstLayer[0])
+		contentArrayOfSecondLayer := search.SearchArrays(bssecond, []string{}...)
 
-	// contentArrayLength := len(contentArrayOfSecondLayer)
-	TopicMaps := make(TopicMap, 15)
-	for _, it := range contentArrayOfSecondLayer {
-		// fmt.Println(it)
-		arybs := utils.I2Bytes(it)
-		ary := search.SearchArrays(arybs, []string{}...)
-		// fmt.Println(ary)
-		for _, its := range ary {
-			// fmt.Println(its)
-			id := search.SearchFIValue(utils.I2Bytes(its), "id", []string{}...)
-			cont := search.SearchSValue(utils.I2Bytes(its), "cont", []string{}...)
-			relate := search.SearchSValue(utils.I2Bytes(its), "relate", []string{}...)
-			parent := search.SearchFIValue(utils.I2Bytes(its), "parent", []string{}...)
-			topic := Topic{id, cont, relate, parent}
-			// fmt.Printf("%v %s\t%s %v\n", id, cont, relate, parent)
-			fmt.Printf("%v\n", topic)
-			TopicMaps[topic.Relate] = topic
-		}
-		fmt.Println(TopicMaps["SBV"], TopicMaps["HED"], TopicMaps["VOB"])
-	}
+		// contentArrayLength := len(contentArrayOfSecondLayer)
+		TopicMaps := make(TopicMap, 15)
+		for _, it := range contentArrayOfSecondLayer {
+			// fmt.Println(it)
+			arybs := utils.I2Bytes(it)
+			ary := search.SearchArrays(arybs, []string{}...)
+			// fmt.Println(ary)
+			for _, its := range ary {
+				// fmt.Println(its)
+				id := search.SearchFIValue(utils.I2Bytes(its), "id", []string{}...)
+				cont := search.SearchSValue(utils.I2Bytes(its), "cont", []string{}...)
+				relate := search.SearchSValue(utils.I2Bytes(its), "relate", []string{}...)
+				parent := search.SearchFIValue(utils.I2Bytes(its), "parent", []string{}...)
+				topic := Topic{id, cont, relate, parent}
+				// fmt.Printf("%v %s\t%s %v\n", id, cont, relate, parent)
+				fmt.Printf("%v\n", topic)
+				TopicMaps[topic.Relate] = topic
+			}
+			fmt.Println(TopicMaps["SBV"], TopicMaps["HED"], TopicMaps["VOB"])
+		}*/
 }
 
-func (self *TopicAction) analyse(data []byte) {
+func (self *TopicAction) analyse(sentence string, data []byte) {
 	// 第一层数组
 	contentArrayOfFirstLayer := search.SearchArrays(data, []string{}...)
 	// fmt.Println(contentArrayOfFirstLayer)
@@ -96,27 +102,51 @@ func (self *TopicAction) analyse(data []byte) {
 	contentArrayOfSecondLayer := search.SearchArrays(bssecond, []string{}...)
 	// fmt.Println(contentArrayOfSecondLayer)
 
-	// contentArrayLength := len(contentArrayOfSecondLayer)
-	TopicMaps := make(TopicMap, 25)
 	for _, it := range contentArrayOfSecondLayer {
 		// fmt.Println(it)
 		arybs := utils.I2Bytes(it)
 		ary := search.SearchArrays(arybs, []string{}...)
 		// fmt.Println(ary)
+		topics := make(TopicSlice, 0)
 		for _, its := range ary {
-			// fmt.Println(its)
 			id := search.SearchFIValue(utils.I2Bytes(its), "id", []string{}...)
 			cont := search.SearchSValue(utils.I2Bytes(its), "cont", []string{}...)
 			relate := search.SearchSValue(utils.I2Bytes(its), "relate", []string{}...)
 			parent := search.SearchFIValue(utils.I2Bytes(its), "parent", []string{}...)
 			topic := Topic{id, cont, relate, parent}
-			// fmt.Printf("%v %s\t%s %v\n", id, cont, relate, parent)
-			fmt.Printf("%v\n", topic)
-			TopicMaps[topic.Relate] = topic
+			topics = append(topics, topic)
 		}
-		fmt.Println(TopicMaps["SBV"], TopicMaps["HED"], TopicMaps["VOB"])
-		stringSaveChan <- TopicMaps["SBV"].Const + TopicMaps["HED"].Const + TopicMaps["VOB"].Const + TopicMaps["POB"].Const + TopicMaps["IOB"].Const + TopicMaps["FOB"].Const
+		stringSaveChan <- sentence
+		stringSaveChan <- processSentence(topics)
 	}
+}
+
+// 处理句子成分
+func processSentence(topicsOrigin TopicSlice) string {
+	TopicMaps := make(TopicMap, len(topicsOrigin))
+	topicsStrOrigin := ""
+	sort.Sort(topicsOrigin)
+	for _, it := range topicsOrigin {
+		TopicMaps[it.Relate] = it
+		topicsStrOrigin += it.String()
+	}
+	topics := make(TopicSlice, 0)
+	id := TopicMaps["HED"].Id
+	topics = append(topics, TopicMaps["HED"])
+	for _, v := range topicsOrigin {
+		if v.Parent == id {
+			topics = append(topics, v)
+		}
+	}
+	sort.Sort(topics)
+	result := ""
+	topicsStr := ""
+	for _, it := range topics {
+		result += it.Const
+		topicsStr += it.String()
+	}
+	// fmt.Println(result)
+	return result + "\n" + topicsStrOrigin + "\n" + topicsStr + "\n"
 }
 
 func (self *TopicAction) Search() {
@@ -130,11 +160,26 @@ func (self *TopicAction) Search() {
 		ipaddr := "202.120.87.152"
 		bs := fetch.Do1(url, ipaddr)
 		self.persis.Do(bs)
-		self.analyse(bs)
+		self.analyse(sentence, bs)
 		time.Sleep(time.Second * 1)
 	}
 }
 
 func (self *TopicAction) Close() {
 	self.persis.MgoDB.Close()
+}
+
+// 排序
+type TopicSlice []Topic
+
+func (c TopicSlice) Len() int {
+	return len(c)
+}
+
+func (c TopicSlice) Less(i, j int) bool {
+	return c[i].Id < c[j].Id
+}
+
+func (c TopicSlice) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
 }
