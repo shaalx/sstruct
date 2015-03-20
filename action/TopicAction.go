f48bc450060d8b97321d1aadbdcfa2ab7f73b440
shaalx <1316011195@qq.com>
2015-03-20 22:49:37 +0800

    time costs

--- action/TopicAction.go
+++ action/TopicAction.go
@@ -2,9 +2,13 @@ package action
 
 import (
 	"github.com/shaalx/sstruct/service"
+	"github.com/shaalx/sstruct/service/log"
+	"time"
 )
 
 func TopicAction() {
+	start := time.Now()
+
 	var serv service.Service
 	serv = &service.TopicAction{}
 	serv.Init()
@@ -12,4 +16,7 @@ func TopicAction() {
 	// serv.Search()
 	serv.Analyse(33)
 	defer serv.Close()
+
+	log.LOGS.Alert("Time costs : %v", time.Now().Sub(start))
+	time.Sleep(time.Second)
 }
