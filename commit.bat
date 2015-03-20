068c6df99c62ffa436cd4fcb95844720d64f480f
shaalx <wisdomswing@gmail.com>
2015-03-20 18:57:03 +0800

    try merge

--- commit.bat
+++ commit.bat
@@ -3,8 +3,8 @@ git status
 ::echo commit for change : 
 set /p commit_for_change=
 git commit -m "%commit_for_change%"
-git push oschina topic:topic
-git push github topic:topic
+git push oschina topic:master
+::git push github topic:master
 ::net stop server
 ping /n 8 127.1 >nul
 ::net start server
