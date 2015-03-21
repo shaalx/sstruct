bd13c9849b3be591ffd0488d38fca6d8a638ee2f
shaalx <wisdomswing@gmail.com>
2015-03-21 16:44:09 +0800

    args

---
+++ bean/args.go
@@ -0,0 +1,8 @@
+package bean
+
+var (
+	ORIGIN_DIR = "./static/origin" // 文件 源目录
+	FILENAME   = "file.txt"        // 正在处理的文件
+	SPILT_DIR  = "./static/spilt"  // 分词结果文件夹
+	STAT_DIR   = "./static/stat"   // 统计结果文件夹
+)
