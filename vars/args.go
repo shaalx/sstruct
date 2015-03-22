e8fa1ac163a60d7807d576ba38a95c74f51054cf
shaalx <1316011195@qq.com>
2015-03-22 21:51:00 +0800

    key

--- vars/args.go
+++ vars/args.go
@@ -1,10 +1,11 @@
 package vars
 
 var (
-	ORIGIN_DIR       = "./static/origin/" // 文件 源目录
-	CURRENT_FILENAME = "file.txt"         // 正在处理的文件
-	SPILT_DIR        = "./static/spilt/"  // 分词结果文件夹
-	STAT_DIR         = "./static/stat/"   // 统计结果文件夹
-	LOG_FILE         = "log.txt"          // 日志文件
-	TEMP_FILENAME    = "./static/temp.txt"
+	ORIGIN_DIR       = "./static/origin/"  // 文件 源目录
+	CURRENT_FILENAME = "file.txt"          // 正在处理的文件
+	SPILT_DIR        = "./static/spilt/"   // 分词结果文件夹
+	STAT_DIR         = "./static/stat/"    // 统计结果文件夹
+	LOG_FILE         = "log.txt"           // 日志文件
+	TEMP_FILENAME    = "./static/temp.txt" // 临时文件：做文件内容转换
+	KEY_DIR          = "./static/key"
 )
