3a20946694eb740f2a222ff36f5157b8a3cc75a7
shaalx <1316011195@qq.com>
2015-03-25 22:32:53 +0800

    file walk test

--- utils/iofile_test.go
+++ utils/iofile_test.go
@@ -1,9 +1,17 @@
 package utils
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestDeleteEnter(t *testing.T) {
-	DeleteEnter("file.txt")
+// func TestDeleteEnter(t *testing.T) {
+// 	DeleteEnter("file.txt")
+// }
+
+func TestReadDir(t *testing.T) {
+	files := ReadDir(".")
+	for i, it := range files {
+		fmt.Printf("%d:\t %s\n", i, it)
+	}
 }
