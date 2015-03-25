f994640f9a2acc89c48c6b8535dd78749973ae9a
shaalx <1316011195@qq.com>
2015-03-25 22:59:33 +0800

    auto analyse

--- action/TopicAction.go
+++ action/TopicAction.go
@@ -12,13 +12,15 @@ var logMapping map[int64]string
 
 func init() {
 	logMapping = make(map[int64]string, 1)
-	logMapping[1426986912] = "C19-Computer0006.txt"
-	logMapping[1426989715] = "C34-Economy0002.txt"
-	logMapping[1426988787] = "C16-Electronics02.txt"
-	logMapping[1426943207] = "file2.txt"
-	logMapping[1426943167] = "file1.txt"
-	logMapping[1427166719] = "file.txt"
-	logMapping[1427101485] = "C32-Agriculture0002.txt"
+	// logMapping[1426986912] = "C19-Computer0006.txt"
+	// logMapping[1426989715] = "C34-Economy0002.txt"
+	// logMapping[1426988787] = "C16-Electronics02.txt"
+	// logMapping[1426943207] = "file2.txt"
+	// logMapping[1426943167] = "file1.txt"
+	// logMapping[1427166719] = "file.txt"
+	// logMapping[1427101485] = "C32-Agriculture0002.txt"
+
+	logMapping[1426948532] = "file2.txt"
 	logMapping[1427031288] = "C34-Economy0002.txt"
 	DATE = 1427031288
 	CURRENT_FILENAME = logMapping[DATE]
@@ -27,7 +29,26 @@ func init() {
 func TopicAction() {
 	// TopicActionAnalyseN()
 	// TopicAction_PersistenceWithUnixDate() // 获得分词
-	TopicAction_AnalyseWithUnixDate() // 分析结果
+	// TopicAction_AnalyseWithUnixDate() // 分析结果
+	AutoAnalyse()
+}
+
+func AutoAnalyse() {
+	start := time.Now()
+
+	var serv service.Service
+	serv = &service.TopicAction{}
+	serv.Init()
+	defer serv.Close()
+
+	for date, file := range logMapping {
+		loopstart := time.Now()
+		CURRENT_FILENAME = file
+		serv.AnalyseWithUnixDate(date)
+		log.LOGS.Alert("Time costs : %v", time.Now().Sub(loopstart))
+	}
+	log.LOGS.Alert("Time costs : %v", time.Now().Sub(start))
+	time.Sleep(time.Second)
 }
 
 func TopicAction_PersistenceWithUnixDate() {
