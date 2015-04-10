55f21fc7a06241202f4f5945df31f908c2c01387
shaalx <wisdomswing@gmail.com>
2015-04-10 20:19:52 +0800

    bat format

--- commit.bat
+++ commit.bat
@@ -1,8 +1,8 @@
 git add -A
 git status
 ::echo commit for change : 
+@echo commit:
 @set /p commit_for_change=
-echo commit:
 git commit -m "%commit_for_change%"
 git push oschina master
 git push github master
